76a630e9e1dfb9aeefa1cee0ed880a728c7d7199
Harish Thokala <hthokala@dev-as-login-1.ci.neoninternal.org>
2018-12-12 15:55:40 -0700

    session longops script

---
+++ Oracle/Space/SESSION_LONGOPS.sql
@@ -0,0 +1,7 @@
+SELECT username,sid, serial#,
+TO_CHAR(CURRENT_TIMESTAMP,'HH24:MI:SS') AS curr,
+TO_CHAR(start_time,'HH24:MI:SS') AS logon,
+(sysdate - start_time)*24*60 AS mins
+FROM V$SESSION_LONGOPS
+WHERE    username is not NULL
+AND (SYSDATE - start_time)*24*60 > 1 ;
